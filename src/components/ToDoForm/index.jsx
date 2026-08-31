55395c1f625c7cfd1656a1a0c31f28fc69fcc1cc
Daniel Muritiba <danielsmuritiba@gmail.com>
2026-08-31 18:38:38 -0300

    form

--- src/components/ToDoForm/index.jsx
+++ src/components/ToDoForm/index.jsx
@@ -2,10 +2,10 @@ import "./todo-form.style.css";
 import { TextInput } from "../TextInput";
 import { Save } from "../Save";
 
-export function TodoForm(onSubmit) {
+export function TodoForm({ onSubmit }) {
   return (
-    <form className="form" action={onSubmit}>
-      <TextInput placeholder="Digite o item que deseja adicionar" />
+    <form className="todo-form" action={onSubmit}>
+      <TextInput placeholder="Digite o item que deseja adicionar" required />
       <Save>Salvar item</Save>
     </form>
   );
